fbaa0ad35ebd419e58db901cea260790833c85d9
Leonardo Palomo <leonardo.palomo@usach.cl>
2019-09-23 12:28:10 -0300

    intento de nueva tarea

--- src/views/NuevaTarea.vue
+++ src/views/NuevaTarea.vue
@@ -9,15 +9,16 @@
               label="Titulo"
               required
             ></v-text-field>
-            <br>
-            <select align="center" name="Voluntarios">
-                <option value="1">Voluntario 1</option>
-                <option value="2">Voluntario 2</option>
-                <option value="1">Voluntario 3</option>
-                <option value="2">Voluntario 4</option>
-                </select>
-                <br>
-
+            <v-text-field
+              v-model="tarea.estado"
+              label="Estado"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="tarea.id_voluntario"
+              label="Voluntario"
+              required
+            ></v-text-field>
 
             <v-btn class="mr-4" @click="save" color = "teal lighten-4">Crear</v-btn>
             <v-btn class="mr-4" to="/tareas">Volver</v-btn>
@@ -35,6 +36,7 @@ export default{
   data:function(){
     return{
       tarea:{},
+      tareaEmergencia: {},
       message:""
     }
   },
@@ -43,6 +45,9 @@ export default{
       this.message = "";
       try {
           let response = await this.$http.post('/tareas', this.tarea);
+          tareaEmergencia.id_emergencia = $route.params.id_emergencia;
+          tareaEmergencia.id_tarea = response.id_tarea;
+          let responseB = await this.$http.post('/emergencia_tarea', this.tareaEmergencia);
           this.message = "Se ha agregado existosamente"
       } catch (e) {
         console.log('error',e)
